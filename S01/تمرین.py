f68f8a7fb27b3e4302dc80b5a8d7b1b6b38a118c
Nima <nimanenaee82@gmail.com>
2024-05-23 15:47:24 +0330

    new

---
+++ S01/تمرین.py
@@ -0,0 +1,43 @@
+"""a=4.65
+b=complex(a)
+b.imag
+b.real
+print(b.imag)
+print(b)
+print(b.real)"""
+
+
+"""a=.1+.1+.1
+print(a)
+"""
+
+
+'''a=('i\'m student')
+print(a)'''
+
+"""a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+print (a % b, " ", a // b)"""
+
+'''
+x=int(input("x ra vared konid:"))
+y=int(input("y ra vared konid:"))
+z= x**3 + 2*x**2*y + 3*y - 7
+print(z)'''
+
+
+'''str1 = input("Enter string1: ")
+str2 = input("Enter string2: ")
+str3= str1+str2
+print(str3)
+'''
+
+'''s=input("yak reshteh ra vared konid:")
+i=int(input("tedad tekrar ra vared konid:"))
+print(s * i)'''
+'''
+x=int(input("x ra vared konid:"))
+y=int(input("y ra vared konid:"))
+complex1= complex(x,y)
+print(complex1)'''
+
--- s02/s02.py
+++ s02/s02.py
@@ -1,1 +1,7 @@
-print("nima")
+# Question 1:
+age1 = int(input("age of person1:"))
+age2= int(input("age of person2:"))
+age3= int(input("age of person3:"))
+oldest_age = max(age1,age2,age3)
+youngest_age = min(age1,age2,age3)
+print("oldest age is:",oldest_age,"youngest age is:",youngest_age)
